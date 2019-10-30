a7816689bd48dd7a6c5f60da836d2219c28d65b8
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-30 14:38:54 +0700

    more layout grid

--- layout_grid.py
+++ layout_grid.py
@@ -7,9 +7,14 @@ txt_pass = Label(root, text="Password")
 inp_name = Entry(root)
 inp_pass = Entry(root)
 
-txt_name.grid(row=0)
-txt_pass.grid(row=1)
+# Align Using "Sticky" With the value N, W, E, S (North-Top, West-Left, South-Bottom, East-Right)
+txt_name.grid(row=0, sticky=E)
+txt_pass.grid(row=1, sticky=E)
 inp_name.grid(row=0, column=1)
 inp_pass.grid(row=1, column=1)
+
+# Checkbox With Column Span
+chk_logg = Checkbutton(root, text="Keep me logged in!")
+chk_logg.grid(columnspan=2)
 
 root.mainloop()
