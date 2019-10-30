3afe6852d471aa2d0f14dbdd3752eb77fc2af976
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-30 13:49:22 +0700

    layout fit

---
+++ layout_fit.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+one = Label(root, text="One", fg="white", bg="black")
+one.pack()
+
+two = Label(root, text="Two", fg="white", bg="green")
+two.pack(fill=X)
+
+three = Label(root, text="One", fg="black", bg="yellow")
+three.pack(side=LEFT, fill=Y)
+
+root.mainloop()
+
